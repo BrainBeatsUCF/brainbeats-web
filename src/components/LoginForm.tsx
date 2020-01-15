c257ac562bc7e0683260cf75e9a9567793515c56
Justin <justin.c.bang@gmail.com>
2020-01-15 17:55:27 -0500

    Fixed routes template with malformed return
    Also updated package.json with Material-UI

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
   
+interface LoginProps {
+	email?: string;
+	password?: string;
+}
+
 // WIP Login Form
-const LoginForm = undefined;
+const LoginForm: React.FC<LoginProps> = ({ ...props }) => {
+	return (
+	  <>
+		<h1>Login</h1>
+	  </>
+	);
+};
   
 export default LoginForm;
   
