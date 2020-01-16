4ecf671aa3f5fc5b24fc0fadd18d9cad4c524f0b
Justin <justin.c.bang@gmail.com>
2020-01-15 23:45:42 -0500

    Imported + Modified Base Home Page Template

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,16 +5,18 @@ import {
     Switch,
   } from 'react-router-dom';
 import Login from '../views/Login';
+import Home from '../views/Home';
 
 const Routes = (): JSX.Element => {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login" component={Login} />
-        </Switch>
-      </BrowserRouter>
-    )
-  };
+  return (
+    <BrowserRouter>
+      <Switch>
+		    <Route path="/" component={Home} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </BrowserRouter>
+  )
+};
   
   export default Routes;
   
---
+++ src/views/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import HomePage from '../components/HomePage';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+};
+
+export default Home;
