6592d210e94ffc1c99faaa01060b6761b5f1ce33
Hung Nguyen <hn9581@gmail.com>
2020-01-15 20:35:13 -0500

    Added register page with form validation

---
+++ src/views/Register.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import RegisterForm from '../components/RegisterForm';
+
+const LoginPage: React.FC = () => {
+    return (
+      <>
+        <RegisterForm />
+      </>
+    );
+  };
+  
+export default LoginPage;
