5a72e51c488b20794f70237f1d22e90f0a2b9ece
Hung Nguyen <hn9581@gmail.com>
2020-02-23 13:18:50 -0500

    Added playlistdetails view

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,10 +5,17 @@ import {
     Switch,
   } from 'react-router-dom';
 import Login from '../views/LoginView';
+<<<<<<< HEAD
 import Home from '../views/HomeView';
 import PlaylistPage from '../views/PlaylistView';
+=======
+import Register from '../views/Register';
+import Dashboard from '../views/DashboardView';
+import PlaylistDetails from '../views/PlaylistDetails';
+>>>>>>> Added playlistdetails view
 
 const Routes = (): JSX.Element => {
+  
   return (
     <BrowserRouter>
       <Switch>
